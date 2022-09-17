dab47fbec5908c87d7fee9099874296a3bcce10c
Prado Neto <manoel.prado.neto@accenture.com>
2022-09-17 19:23:43 -0300

    learning how debug

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,14 @@ import createCourse from './routes';
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', createCourse);
+
+app.post('/courses', (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
 
 app.listen(3333, () => console.log("server is running"));
