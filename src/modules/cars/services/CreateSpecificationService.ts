d6eb0df44d7496e4426e2c3ff5d206049ac24dcc
Prado Neto <manoel.prado.neto@accenture.com>
2022-09-20 14:29:33 -0300

    is not possible to create two specifications with the same name

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -9,6 +9,12 @@ class CreateSpecificationService {
   constructor(private specificationsRepository: ISpecificationsRepository) {}
 
   execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists)
+      throw new Error('Specification already exists!');
+
     this.specificationsRepository.create({
       name,
       description,
