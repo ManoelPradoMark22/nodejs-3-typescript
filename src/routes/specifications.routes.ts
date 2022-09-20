776f683a61320f4d79e27c8730ed7b3822e93622
Prado Neto <manoel.prado.neto@accenture.com>
2022-09-20 14:42:18 -0300

    specifications.routes & list specifications

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,27 @@
+import { response, Router } from 'express';
+
+import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
+import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+
+const specificationsRoutes = Router();
+
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post('/', (req, res) => {
+  const { name, description } = req.body;
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository,
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return res.status(201).send();
+});
+
+specificationsRoutes.get('/', (req, res) => {
+  const allCategories = specificationsRepository.list();
+
+  return res.json(allCategories);
+});
+
+export { specificationsRoutes };
