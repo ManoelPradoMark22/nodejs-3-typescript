4678915086474bf0d17d8345cc1df1c2e373880c
Manoel Prado <manoelprado.aecjr@gmail.com>
2022-09-21 11:38:59 -0300

    using stream to read file - use csv-parse to read lines from folder

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,18 @@
+import { parse as csvParse } from 'csv-parse';
+import fs from 'fs';
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    // csv, por padrão, já reconhece o separador sendo a vírgula
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async line => {
+      console.log(line);
+    });
   }
 }
 
