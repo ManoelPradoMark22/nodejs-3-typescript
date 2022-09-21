547fbbd13e8beeba7a8924956533bf1a5e704a1b
Manoel Prado <manoelprado.aecjr@gmail.com>
2022-09-21 18:50:50 -0300

    removing files after create categories with file system

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -30,7 +30,10 @@ class ImportCategoryUseCase {
             description,
           });
         })
-        .on('end', () => resolve(categories))
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
         .on('error', err => reject(err));
     });
   }
